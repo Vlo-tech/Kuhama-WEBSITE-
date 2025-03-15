e2f86bd4cdf611b9cfd2cc2cb93dc7e26defa0f4
Vlo-tech <jm7903739@gmail.com>
2025-03-15 13:38:02 +0300

    2 time trying to fix vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'public', // Ensure this path points to the directory containing index.html
+  root: 'my-app/public', // Ensure this path points to the directory containing index.html
   build: {
-    outDir: '../dist', // Output directory for the build
+    outDir: '../../dist', // Output directory for the build
     rollupOptions: {
-      input: 'public/index.html', // Ensure the path to index.html is correct
+      input: 'my-app/public/index.html', // Ensure the path to index.html is correct
     },
   },
 })
